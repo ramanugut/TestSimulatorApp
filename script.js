b8b9cb9fcac6212ada004631373c4b22efba25a4
Thembuluwo Ramanugu <42994454+ramanugut@users.noreply.github.com>
2024-10-22 08:25:31 +0200

    Update script.js
      // Test file references
      const testFiles = [
        // Add your test files here
        // "test20.json",
        "test21.json",
        "test22.json",
        "test23.json",
        "test24.json",
        "test25.json",
        "test26.json",
        "test27.json",
        "test28.json",
        // For demonstration, we'll use a sample test file
        // "sample_test.json",
      ];

--- script.js
+++ script.js
@@ -175,6 +175,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "test25.json",
     "test26.json",
     "test27.json",
+    "test28.json",
     // For demonstration, we'll use a sample test file
     // "sample_test.json",
   ];
